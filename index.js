c06c5ea5fbbb55b098ab2710f04f9a573581f893
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2022-10-31 17:58:57 +0530

    added videos

--- index.js
+++ index.js
@@ -2,24 +2,3 @@
 import './style.css';
 
 // Write Javascript code!\
-const video = document.getElementById('myVideo');
-video.addEventListener('canplaythrough', (e) => {
-  var i = 0;
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById('myBar');
-    var width = 1;
-    var id = setInterval(frame, 200);
-    function frame() {
-      if (video.ended || width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        // console.log(video.currentTime);
-        // console.log(video.duration);
-        width = Math.ceil((video.currentTime / video.duration) * 100);
-        elem.style.width = width + '%';
-      }
-    }
-  }
-});
