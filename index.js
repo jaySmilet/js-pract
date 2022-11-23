8347a6a4951005c1878dddaa575fe4da92d6e673
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2022-11-23 19:54:46 +0530

    2 files modified

--- index.js
+++ index.js
@@ -3,4 +3,30 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const obj = document.getElementById('obj');
+
+var expressionObj = {
+  angry: 0.000006727961135766236,
+  disgusted: 9.93469129184632e-9,
+  fearful: 0.000015661238649045117,
+  happy: 0.7185803055763245,
+  neutral: 0.2799791991710663,
+  sad: 0.000041106035496341065,
+  surprised: 0.0013768896460533142,
+};
+
+var max_exp_name = Object.keys(expressionObj)[0];
+var max_exp_value = Object.values(expressionObj)[0];
+
+for (let key in expressionObj) {
+  if (expressionObj[key] >= max_exp_value) {
+    max_exp_value = expressionObj[key];
+    max_exp_name = key;
+  }
+}
+
+console.log(max_exp_name, max_exp_value);
+
+obj.innerHTML = `${JSON.stringify(expressionObj)}`;
+
+appDiv.innerHTML = `${max_exp_name}-${max_exp_value}`;
