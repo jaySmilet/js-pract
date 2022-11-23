c862267dc20c06009cd556b6bf8d3eb8d7f9f381
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2022-11-24 00:44:49 +0530

    1 file modified

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ video.addEventListener('canplaythrough', (e) => {
         clearInterval(id);
         i = 0;
       } else {
-        console.log(video.currentTime % 3);
+        console.log(video.currentTime);
         // console.log(video.duration);
         width = Math.ceil((video.currentTime / video.duration) * 100);
         elem.style.width = width + '%';
